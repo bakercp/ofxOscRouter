92ac222a4693e9f10b887b12935eba22dfa1d574
Christopher Baker <me@christopherbaker.net>
2012-07-18 16:34:42 -0500

    Added testing info.

--- lib/osc_match/src/osc_match.c
+++ lib/osc_match/src/osc_match.c
@@ -1,39 +1,39 @@
 /*
-Written by John MacCallum, The Center for New Music and Audio Technologies,
-University of California, Berkeley.  Copyright (c) 2009, The Regents of
-the University of California (Regents). 
-Permission to use, copy, modify, distribute, and distribute modified versions
-of this software and its documentation without fee and without a signed
-licensing agreement, is hereby granted, provided that the above copyright
-notice, this paragraph and the following two paragraphs appear in all copies,
-modifications, and distributions.
-
-IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
-SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
-OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
-BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
-MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-*/
+ Written by John MacCallum, The Center for New Music and Audio Technologies,
+ University of California, Berkeley.  Copyright (c) 2009, The Regents of
+ the University of California (Regents). 
+ Permission to use, copy, modify, distribute, and distribute modified versions
+ of this software and its documentation without fee and without a signed
+ licensing agreement, is hereby granted, provided that the above copyright
+ notice, this paragraph and the following two paragraphs appear in all copies,
+ modifications, and distributions.
+ 
+ IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+ BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
 #include "osc_match.h"
 
 int osc_match(const char *pattern, const char *address, int *pattern_offset, int* address_offset) {
-
+    
 	int r;
-
+    
 	const char *pattern_start;
 	const char *address_start;
-
+    
 	pattern_start = pattern;
 	address_start = address;
-
+    
 	*pattern_offset = 0;
 	*address_offset = 0;
-
+    
 	while(*address != '\0' && *pattern != '\0'){
 		if(*pattern == '*'){
 			if(!osc_match_star(pattern, address)){
@@ -58,20 +58,20 @@ int osc_match(const char *pattern, const char *address, int *pattern_offset, int
 			address++;
 		}
 	}
-
+    
 	*pattern_offset = pattern - pattern_start;
 	*address_offset = address - address_start;
-
+    
 	r = 0;
-
+    
 	if(*address == '\0') {
 		r |= OSC_MATCH_ADDRESS_COMPLETE;
 	}
-
+    
 	if(*pattern == '\0') {
 		r |= OSC_MATCH_PATTERN_COMPLETE;
 	}
-
+    
 	return r;
 }
 
@@ -92,7 +92,7 @@ int osc_match_star(const char *pattern, const char *address){
 	pattern--;
 	address--;
 	switch(num_stars){
-	case 1:
+        case 1:
 		{
 			const char *pp = pattern, *aa = address;
 			while(*pp != '*'){
@@ -109,8 +109,8 @@ int osc_match_star(const char *pattern, const char *address){
 				aa--;
 			}
 		}
-		break;
-	case 2:
+            break;
+        case 2:
 #if (OSC_MATCH_ENABLE_2STARS == 1)
 		{
 			const char *pp = pattern, *aa = address;
@@ -153,16 +153,16 @@ int osc_match_star(const char *pattern, const char *address){
 			}
 			return 0;
 		}
-		break;
+            break;
 #else
-		return 0;
+            return 0;
 #endif
-	default:
+        default:
 #if (OSC_MATCH_ENABLE_NSTARS == 1)
-		return osc_match_star_r(pattern_start, address_start);
-		break;
+            return osc_match_star_r(pattern_start, address_start);
+            break;
 #else
-		return 0;
+            return 0;
 #endif
 	}
 	return 1;
@@ -201,28 +201,28 @@ int osc_match_star_r(const char *pattern, const char *address){
 
 int osc_match_single_char(const char *pattern, const char *address){
 	switch(*pattern){
-	case '[':
-		return osc_match_bracket(pattern, address);
-	case ']':
-		while(*pattern != '['){
-			pattern--;
-		}
-		return osc_match_bracket(pattern, address);
-	case '{':
-		return osc_match_curly_brace(pattern, address);
-	case '}':
-		while(*pattern != '{'){
-			pattern--;
-		}
-		return osc_match_curly_brace(pattern, address);
-	case '?':
-		return 1;
-	default:
-		if(*pattern == *address){
-			return 1;
-		}else{
-			return 0;
-		}
+        case '[':
+            return osc_match_bracket(pattern, address);
+        case ']':
+            while(*pattern != '['){
+                pattern--;
+            }
+            return osc_match_bracket(pattern, address);
+        case '{':
+            return osc_match_curly_brace(pattern, address);
+        case '}':
+            while(*pattern != '{'){
+                pattern--;
+            }
+            return osc_match_curly_brace(pattern, address);
+        case '?':
+            return 1;
+        default:
+            if(*pattern == *address){
+                return 1;
+            }else{
+                return 0;
+            }
 	}
 	return 0;
 }
@@ -250,17 +250,17 @@ int osc_match_bracket(const char *pattern, const char *address){
 	}
 	return matched;
 	/*
-	if(matched){
-		while(*pattern != ']' && *pattern != '\0'){
-			pattern++;
-		}
-		pattern++;
-		address++;
-	}else{
-		return 0;
-	}
-	return 1;
-	*/
+     if(matched){
+     while(*pattern != ']' && *pattern != '\0'){
+     pattern++;
+     }
+     pattern++;
+     address++;
+     }else{
+     return 0;
+     }
+     return 1;
+     */
 }
 
 int osc_match_curly_brace(const char *pattern, const char *address){
@@ -277,7 +277,7 @@ int osc_match_curly_brace(const char *pattern, const char *address){
 			}
 		}
 	}
-
+    
 	if(matched){
 		while(*pattern != '}' && *pattern != '\0' && *pattern != '/'){
 			pattern++;
@@ -287,62 +287,47 @@ int osc_match_curly_brace(const char *pattern, const char *address){
 	}else{
 		return 0;
 	}
-
+    
 	return 1;
 }
 
-/*
 
-#include "stdio.h"
-
-int main() {
-
-	char* a[6] = {
-		"/foo",
-		"/foo/bar",
-		"/foo/bar/baz",
-		"/fu",
-		"/foo/bar/bing/ping/8/3",
-		"/foo/jdlkalsdjalsjd/bing/8/lkj 890"
-	};
-
-	int rootLevels = 1;
-	char* root[rootLevels] = {
-		"/livedraw"
-	};
-	
-	int secondLevels = 3;
-	char * layers[secondLevels] = {
-		"/layout",
-		"/session",
-		"/layers"
-	}
-	
-	
-	// full path
-	
-	
-	
-
-	int t;
-	int q;
-	int r;
-
-	int i;
-
-	for (i = 0; i < 6; i++) {
-		r = osc_match(p, a[i], &t, &q);
-		printf("in=>%s dest=>%s => &t=%s &q=%s R=(%d)\n", p, a[i], p+t, a[i]+q, r);
-	}
-	
-	// const char *pattern, const char *address, int *pattern_offset, int* address_offset
-	
-	
-	
-
-	return 1;
-}
-
-*/
-
+ 
+// #include "stdio.h"
+// 
+// int main() {
+// 
+//
+// char* address_to_test[6] = {
+// "/foo",
+// "/foo/bar",
+// "/foo/bar/baz",
+// "/fu",
+// "/foo/baz/bing/ping/8/3",
+// "/foo/jdlkalsdjalsjd/bing/8/lkj 890"
+// };
+//  
+// int pattern_offset;
+// int address_offset;
+// int result;
+// 
+// int i;
+//     
+//     
+//char* pattern = "/*/ba* boogie 8 3 4";
+// 
+// for (i = 0; i < 6; i++) {
+//     result = osc_match(pattern, address_to_test[i], &pattern_offset, &address_offset);
+//     
+//     printf("pattern=>%s address_to_test=>%s => pattern_offset=%s address_offset=%s R=(%d)\n", pattern, address_to_test[i], pattern+pattern_offset, address_to_test[i]+address_offset, result);
+// }
+// 
+// // const char *pattern, const char *address, int *pattern_offset, int* address_offset
+// 
+// 
+// 
+// 
+// return 1;
+// }
+// 
 
