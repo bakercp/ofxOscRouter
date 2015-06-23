b037965772b5db241479d201eab8e81ac8c628d1
Christopher Baker <me@christopherbaker.net>
2015-06-23 16:16:28 -0500

    Lots of updates to project / makefiles.

---
+++ addon_config.mk
@@ -0,0 +1,8 @@
+meta:
+	ADDON_NAME = ofxOscRouter
+	ADDON_DESCRIPTION = Addon for routing and otherwise organizing osc messages.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "networking"
+	ADDON_URL = http://github.com/bakercp/ofxOSCRouter
+common:
+	ADDON_DEPENDENCIES = ofxOsc
